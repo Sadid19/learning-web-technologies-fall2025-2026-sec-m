02814cfda10e51593c8b4f19ab4c0641da2ed987
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2025-12-31 13:36:03 +0600

    DB Practice

---
+++ DB_Practice/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$hostName = "127.0.0.1";
+$userName = "root";
+$password = NULL;
+$dbName = "practiceDB";
+
+$con = mysqli_connect($hostName, $userName, $password, $dbName);
+
+if($con){
+    echo "Connection Successful";
+}
+else{
+    echo "Connection failed!";
+    die(mysql_connect_error());
+}
+
+?>
